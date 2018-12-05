bf9fd5755d071a861e9503097de26b84e9a24cce
常帅 <550992499@qq.com>
2018-12-05 16:33:31 +0800

    初识T4【静态，动态】

---
+++ T4模板/TestT4Tempalte/TestT4Tempalte/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestT4Tempalte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RuntimeTextTemplate1 runtimeText = new RuntimeTextTemplate1();
+            string str = runtimeText.TransformText();
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ T4模板/TestT4Tempalte/TestT4Tempalte/TextTemplate1.cs
@@ -0,0 +1,11 @@
+﻿
+public class MyClass
+{
+
+    public int Name { get; set; }
+    public int Age { get; set; }
+    public int Sex { get; set; }
+
+
+
+}

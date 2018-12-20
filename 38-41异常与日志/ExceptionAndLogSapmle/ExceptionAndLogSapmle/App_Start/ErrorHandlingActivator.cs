d3d9dc26a03766c42d0dc601ec49797584a712e9
常帅 <550992499@qq.com>
2018-12-20 18:33:42 +0800

    异常

---
+++ 38-41异常与日志/ExceptionAndLogSapmle/ExceptionAndLogSapmle/App_Start/ErrorHandlingActivator.cs
@@ -0,0 +1,50 @@
+﻿using ExceptionAndLogSapmle.App_Start;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using System;
+using System.Net;
+using System.Web;
+
+[assembly: PreApplicationStartMethod(typeof(ErrorHandlingActivator), "Start")]
+namespace ExceptionAndLogSapmle.App_Start
+{
+    /// <summary>
+    /// 异常处理激活器
+    /// </summary>
+    public static class ErrorHandlingActivator
+    {
+        public static void Start()
+        {
+            //动态注册HttpMoudle
+            DynamicModuleUtility.RegisterModule(typeof(ErrorHandlingStartupModule));
+        }
+    }
+
+
+    //实现httpmodel，http管道模型
+    public class ErrorHandlingStartupModule : IHttpModule
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.Error += Context_Error;
+        }
+
+        private void Context_Error(object sender, System.EventArgs e)
+        {
+            HttpApplication httpApplication = (HttpApplication)sender;
+            //获得异常信息
+            Exception exception = httpApplication.Server.GetLastError();
+            //实例化一个http请求内发生的对象
+            HttpException httpException = new HttpException(null, exception);
+            //http相应状态码
+            if (httpException.GetHttpCode() == (int)HttpStatusCode.NotFound)
+            {
+
+            }
+
+        }
+    }
+}
--- 38-41异常与日志/ExceptionAndLogSapmle/ExceptionAndLogSapmle/Controllers/HomeController.cs
+++ 38-41异常与日志/ExceptionAndLogSapmle/ExceptionAndLogSapmle/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExceptionAndLogSapmle.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,7 @@ namespace ExceptionAndLogSapmle.Controllers
 {
     public class HomeController : Controller
     {
-        //[HandleErrorAttribute(ExceptionType = typeof(DivideByZeroException), Master = null, Order = 1, View = "Error2")]
+        [CustomHandleError]
         public ActionResult Index()
         {
             int a = 1;
